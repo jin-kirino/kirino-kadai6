2d9acd24d708d1bba486f70ff713fb57992c4990
jin-kirino <jinkirino@gmail.com>
2023-04-04 22:45:05 +0900

    arc4random_uniformを使った乱数生成方法を検証

--- kadai6-trail-run.playground/Contents.swift
+++ kadai6-trail-run.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// arc4random_uniform(_ __upper_bound: UInt32)
+// Swift4.1以前の乱数生成方法
+
+// 0から4を100回抽出
+for _ in 0...100 {
+    arc4random_uniform(5)
+    print(arc4random_uniform(5))
+}
+print("-----------------------")
+// 0から100を100回ランダムで抽出
+for _ in 1...100 {
+    arc4random_uniform(100)
+    print(arc4random_uniform(100))
+}
