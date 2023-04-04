b1c66208348260fbc92dbb81ec4c2d8aa0804ea2
jin-kirino <jinkirino@gmail.com>
2023-04-05 08:27:02 +0900

    新しいファイルの生成？

---
+++ kadai6-trail-run.playground/Resources/kirino-kadai6/kadai6-trail-run.playground/Resources/kirino-kadai6/kadai6-trail-run.playground/Resources/kirino-kadai6/kadai6-trail-run.playground/Resources/kirino-kadai6/kirino-kadai6/kirino_kadai6App.swift
@@ -0,0 +1,17 @@
+//
+//  kirino_kadai6App.swift
+//  kirino-kadai6
+//
+//  Created by 神　樹里乃 on 2023/03/22.
+//
+
+import SwiftUI
+
+@main
+struct kirino_kadai6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
