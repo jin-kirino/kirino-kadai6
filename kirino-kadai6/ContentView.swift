e6a683d5e9e0f37915f93a3b6463e913cec351fb
jin-kirino <jinkirino@gmail.com>
2023-04-05 07:00:15 +0900

    #2 判定ボタンタップでアラートを表示する

--- kirino-kadai6/ContentView.swift
+++ kirino-kadai6/ContentView.swift
@@ -10,14 +10,16 @@ import SwiftUI
 struct ContentView: View {
     @State private var correctNumber: Int = 33
     @State private var currentValue: Double = 50.0
+    @State private var isShowingAlert: Bool = false
+    @State private var alertMessage: String = ""
 
     var body: some View {
         VStack {
             Text("\(correctNumber)")
                 .font(.largeTitle)
                 .padding(.bottom, 70.0)
-//            Text("現在の値は、\(currentValue)")
-            Slider(value: $currentValue, in: 0 ... 100)
+            Text("現在の値は、\(currentValue)")
+            Slider(value: $currentValue, in: 0...100)
             HStack {
                 Text("1")
                 Spacer()
@@ -25,13 +27,26 @@ struct ContentView: View {
             }
             Button {
                 print("判定ボタンがタップされたよ！")
+                print("正解の値：\(correctNumber)")
+                print("スライダーの値：\(currentValue)")
+                isShowingAlert = true
             } label: {
                 Text("判定！")
             }
             .padding(.top, 30.0)
             Spacer()
         }
+        .alert("結果", isPresented: $isShowingAlert) {
+            Button("再挑戦") {
+                correctNumber = Int.random(in: 0 ..< 100)
+            }
+        } message: {
+            Text("\(alertMessage)")
+        }
         .padding()
+        .onAppear {
+            correctNumber = Int.random(in: 1 ... 100)
+        }
     }
 }
 
